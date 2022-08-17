24cfec33b061b16a2dd824c1ce1ea32095c4d7b8
gityzon <109445070+gityzon@users.noreply.github.com>
2022-08-17 16:15:04 +0800

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -4,16 +4,6 @@
     pkgs.bashInteractive
     pkgs.php74
     pkgs.php74Extensions.pdo
-    pkgs.php74Extensions.curl
-    pkgs.php74Extensions.ctype
-    pkgs.php74Extensions.dom
-    pkgs.php74Extensions.json
-    pkgs.php74Extensions.mbstring
-    pkgs.php74Extensions.openssl
-    pkgs.php74Extensions.session 
-    pkgs.php74Extensions.simplexml
-    pkgs.php74Extensions.xml
-    pkgs.php74Extensions.zip
-    pkgs.php74Extensions.yaml
+    pkgs.sqlite
     ];
 }
